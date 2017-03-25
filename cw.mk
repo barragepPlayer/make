7da93a9714ac6f73be53edcbef9ac806442f7b63
wangyaoguang <382689420@qq.com>
2017-03-25 11:41:55 +0800

    modify cw.mk

--- cw.mk
+++ cw.mk
@@ -1,9 +1,11 @@
 #Start the dev AryaServer
+# ruby on rails
 Arya.start:
 	@cd ~/topDMC/Arya-API/ && rails s
 .PHONY: Arya.start
 
 #Start the dev cw-apiServer
+#node
 cw.start:
 	@cd ~/topDMC/cw-api/ && npm run debug
 .PHONY: cw.start
